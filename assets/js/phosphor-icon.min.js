a1b4be2d274fa6f2873c89c33136a185c2fe40a4
alienssofttech-blip <alienssoft.tech@gmail.com>
2025-10-01 00:26:53 +0300

    Start repository

---
+++ assets/js/phosphor-icon.min.js
@@ -0,0 +1,1 @@
+var head=document.getElementsByTagName("head")[0];for(const e of["regular","thin","light","bold","fill","duotone"]){var link=document.createElement("link");link.rel="stylesheet",link.type="text/css",link.href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/"+e+"/style.css",head.appendChild(link)}
